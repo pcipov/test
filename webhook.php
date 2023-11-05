926eb7c6cac4ec162edd5d7d070c9886621d5ea5
Peter Cipov <peter@ito.sk>
2023-11-05 15:47:08 +0100

    Webhook

---
+++ webhook.php
@@ -0,0 +1,41 @@
+<?php
+
+
+// SETTINGS
+const REPOSITORY = 'https://github.com/pcipov/latexmania.git';
+const LOGIN = 'pcipov';
+const TOKEN = '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';
+const BRANCH = 'production';
+const DIR = '~/latexmania.sk/web';
+
+
+// CHECK BRANCH
+if (isset($_POST['payload'])) {
+    $data = (array)json_decode($_POST['payload']);
+    $array = explode('/', $data['ref']);
+    if (trim(end($array)) != BRANCH)
+        exit;
+}
+
+
+// CHANGE DIRECTORY
+command('cd ' . DIR);
+
+// GIT ADD
+command('git fetch');
+
+// GIT RESET
+command('git reset --hard');
+
+// GIT LOG
+command('git log -1');
+
+
+
+
+function command($command, $log = true) {
+    $output = shell_exec($command);
+    if($log) {
+        echo strtr($command, [TOKEN=>'TOKEN']) . '<br>' . $output . '<br>';
+    }
+}
