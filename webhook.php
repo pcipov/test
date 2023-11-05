d001a6d60e8b825c6f18e3b500c51e459af6329f
Peter Cipov <peter@ito.sk>
2023-11-05 16:45:19 +0100

    Webhook changes

--- webhook.php
+++ webhook.php
@@ -2,40 +2,39 @@
 
 
 // SETTINGS
-const REPOSITORY = 'https://github.com/pcipov/test.git';
-const LOGIN = 'pcipov';
-const TOKEN = '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';
-const BRANCH = 'master';
-const DIR = '~/latexmania.sk/sub/test';
+const GITHUB_REPOSITORY = 'https://github.com/pcipov/test.git';
+const GITHUB_LOGIN = 'pcipov';
+const GITHUB_TOKEN = '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';
+const GITHUB_BRANCH = 'master';
+const LOCAL_DIR = '~/latexmania.sk/sub/test';
+const LOCAL_TOKEN = 'QNj3x2FmtMpkRzb';
+const LOG = true;
 
+// CHECK LOCAL TOKEN
+if( $_GET['token'] !== LOCAL_TOKEN ) {
+    exit;
+}
 
 // CHECK BRANCH
 if (isset($_POST['payload'])) {
     $data = (array)json_decode($_POST['payload']);
     $array = explode('/', $data['ref']);
-    if (trim(end($array)) != BRANCH)
+    if (trim(end($array)) != GITHUB_BRANCH)
         exit;
 }
 
 
-// CHANGE DIRECTORY
-command('cd ' . DIR);
-
-// GIT ADD
-command('git fetch');
-
-// GIT RESET
-command('git reset --hard');
-
-// GIT LOG
-command('git log -1');
+$commands = [
+    'cd ' . LOCAL_DIR,
+    'git fetch',
+    'git reset --hard',
+    'git log -1'
+];
 
 
-
-
-function command($command, $log = true) {
+foreach($commands as $command) {
     $output = shell_exec($command);
-    if($log) {
-        echo strtr($command, [TOKEN=>'TOKEN']) . '<br>' . $output . '<br>';
+    if(LOG) {
+        echo strtr($command, [GITHUB_TOKEN=>'TOKEN']) . '<br>' . $output . '<br>';
     }
 }
